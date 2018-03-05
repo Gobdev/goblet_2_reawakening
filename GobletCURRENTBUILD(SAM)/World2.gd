2e4afcd2f21318c6d2b5e46980a936adee151bea
Johan Ribom <johan.ribom@hotmail.com>
2018-03-05 19:54:24 +0100

    :construction: Add delay to music in World2

---
+++ GobletCURRENTBUILD(SAM)/World2.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	var t = Timer.new()
+	t.set_wait_time(1)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	yield(t, "timeout")
+	t.queue_free()
+	self.get_node("Music").play()
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass

ec71b748e89379230fd67235b157726723893403
SELANTZ15879011 <samlantz99@gmail.com>
2018-03-06 17:31:07 +0100

    :construction: Add Cinematic Cam for Boss Fight

--- GobletCURRENTBUILD(SAM)/Scripts/CameraBossRoom.gd
+++ GobletCURRENTBUILD(SAM)/Scripts/CameraBossRoom.gd
@@ -7,7 +7,7 @@ extends Camera2D
 func _ready():
 	set_drag_margin(MARGIN_LEFT, -0.2)
 	set_limit(MARGIN_LEFT, -1500)
-	set_limit(MARGIN_RIGHT, 2000)
+	set_limit(MARGIN_RIGHT, 3000)
 	
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -18,6 +18,26 @@ func _ready():
 
 
 
-func _on_CamZoom_body_exited(body):
+
+func _on_CamZoom_body_entered(body):
 	set_zoom(Vector2(2, 2))
 	pass # replace with function body
+
+
+func _on_CamZoom_body_exited(body):
+	set_zoom(Vector2(1.25, 1.25))
+	set_offset(Vector2(0, 0))
+	set_drag_margin(MARGIN_TOP, 0.6)
+	pass # replace with function body
+
+
+func _on_CamRaise_body_entered(body):
+	set_offset(Vector2(400, -200))
+	set_drag_margin(MARGIN_TOP, 1)
+	pass # replace with function body
+
+
+func _on_CamRaise2_body_entered(body):
+	set_offset(Vector2(0, 0))
+	pass # replace with function body
+

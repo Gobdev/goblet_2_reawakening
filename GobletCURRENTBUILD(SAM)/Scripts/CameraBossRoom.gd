713c0923b193f784ea0f9244338335e02280de19
SELANTZ15879011 <samlantz99@gmail.com>
2018-03-06 15:50:48 +0100

    :bug: Add Camera Signal to make Cam zoom out
    Not working. Maybe signal has to send only once.

---
+++ GobletCURRENTBUILD(SAM)/Scripts/CameraBossRoom.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	set_drag_margin(MARGIN_LEFT, -0.2)
+	set_limit(MARGIN_LEFT, -1500)
+	set_limit(MARGIN_RIGHT, 2000)
+	
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+
+
+func _on_CamZoom_body_exited(body):
+	set_zoom(0.5, 0.5)
+	pass # replace with function body
